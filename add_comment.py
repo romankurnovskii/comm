3e14cc22089147470da9c8258d7d6b52175ddd16
Roman Kurnovskii <r.kurnovskii@gmail.com>
2022-06-11 17:04:26 +0300

    init

--- add_comment.py
+++ add_comment.py
@@ -1,18 +1,22 @@
 import sys
 import json
 from datetime import datetime
-arguments = sys.argv
-file_name = arguments[1]
-author = arguments[2]
-message = arguments[3]
+import argparse
 
-print(arguments)
+parser = argparse.ArgumentParser("parser")
+parser.add_argument("file_name", help="")
+parser.add_argument("author", help="")
+parser.add_argument("message", help="")
 
-a_file = open(file_name, "r")
+args = parser.parse_args()
+
+print(args)
+
+a_file = open(args.file_name, "r")
 json_object = json.load(a_file)
 a_file.close()
-json_object.append({"name": author, "message": message,
+json_object.append({"author": args.author, "message": args.message,
                    "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
-a_file = open(file_name, "w")
+a_file = open(args.file_name, "w")
 json.dump(json_object, a_file)
 a_file.close()
